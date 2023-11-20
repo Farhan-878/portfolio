b492218ec965916066a09d03daab29ba01beefe9
Farhan Manzer <145482129+Farhan-878@users.noreply.github.com>
2023-11-20 15:11:18 +0530

    Update Resume.jsx

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -35,7 +35,7 @@ const Resume = () => {
               {/* design */}
               <div className=" flex flex-col gap-1 sm:gap-1 border-2 border-yellow-400 shadow-[0px_0px_16px_1px_rgba(0,0,0,0.1)] p-3 rounded-lg">
                 <h1 className="text-[1.4rem] font-semibold sm:text-xl">
-                  Software Developer
+                  Frontend Developer
                 </h1>
                 <span className=" text-[.9rem] font-semibold text-gray-500 sm:text-base">
                   Websultanate Software Technologies Pvt. Ltd.
