2c613c26bdb864e98d9cd7e725ecddca6a4c0995
Farhan-Dev <syed@websultanate.com>
2024-01-06 17:31:58 +0530

    resume

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,7 +8,7 @@ import { FaFacebook, FaLinkedinIn } from "react-icons/fa";
 import { FiDownload } from "react-icons/fi";
 import { FaPlay } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
-// import resumePDF from "../../assets/Resume.pdf";
+import resumePDF from "../../assets/FarhanResume.pdf";
 import skill from "../../assets/Full Stack Developer2.png";
 import mine from "../../assets/banner.png";
 
