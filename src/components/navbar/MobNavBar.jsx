cc45486f284eb798a2ba83774e60acddc07935b3
Farhan-Dev <farhanmanzer878@gmail.com>
2025-04-30 15:01:06 +0530

    update the icons

--- src/components/navbar/MobNavBar.jsx
+++ src/components/navbar/MobNavBar.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { HiHome, HiUser, HiBriefcase, HiDocumentText, HiMail } from 'react-icons/hi';
+import { FcVoicePresentation } from "react-icons/fc";
+import { FcBriefcase } from "react-icons/fc";
+import { FcPortraitMode } from "react-icons/fc";
+import { FcNook } from "react-icons/fc";
+import { FcHome } from "react-icons/fc";
 
 const icons = [
-    { id: 0, name: 'skills', icon: <HiUser /> },
-    { id: 1, name: 'works', icon: <HiBriefcase /> },
-    { id: 2, name: 'home', icon: <HiHome /> },
-    { id: 3, name: 'resume', icon: <HiDocumentText /> },
-    { id: 4, name: 'contact', icon: <HiMail /> },
+    { id: 0, name: 'skills', icon: <FcPortraitMode /> },
+    { id: 1, name: 'works', icon: <FcBriefcase /> },
+    { id: 2, name: 'home', icon: <FcHome /> },
+    { id: 3, name: 'resume', icon: <FcNook /> },
+    { id: 4, name: 'contact', icon: <FcVoicePresentation /> },
 ];
 
 const scrollToSection = (sectionId) => {
@@ -26,8 +31,10 @@ const MobNavBar = () => {
                 className="hidden md:flex fixed bottom-4 left-1/2 -translate-x-1/2 bg-[#0f111c] px-4 py-4 rounded-xl items-center justify-between gap-4 z-50 shadow-lg"
                 style={{
                     width: '90%',
-                    boxShadow: '#c4f000 0px 2px 0px 0px inset',
-                    backgroundColor: '#111827',
+                    // boxShadow: '#c4f000 0px 2px 0px 0px inset',
+                    // backgroundColor: '#111827',
+                    backgroundColor: "#1b2135",
+                    boxShadow: 'inset 0 2px 0 0 #1d2338',
                 }}
             >
                 {icons.map((item) => (
@@ -37,7 +44,10 @@ const MobNavBar = () => {
                         className={`flex items-center justify-center rounded-md transition
               ${item.name === 'home'
                                 ? 'bg-[#a3e635] text-white px-4 py-2 text-lg font-semibold'
-                                : 'bg-[#1c1e2d] text-white text-xl p-2 w-10 h-10'}`}
+                                : 'bg-[#22283f] text-white text-xl p-2 w-10 h-10'}`}
+                        style={{
+                            boxShadow: "inset 0 2px 0 0 #242b45, 0 12px 16px 0 rgba(0, 0, 0, .06)",
+                        }}
                     >
                         {item.icon}
                         {item.name === 'home' && <span className="ml-2">Home</span>}
